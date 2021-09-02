a60a7096cd9641657d8a96689c286d4cb90882f1
Ariel Talbot <Ariel.Talbot@iconfitness.com>
2021-09-01 22:09:55 -0600

    add local storage hook and review field

---
+++ src/components/Book.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+
+const Book = ({
+  id,
+  bookname,
+  author,
+  review,
+  date,
+  handleRemoveBook
+}) => {
+  return (
+    <Card style={{ width: '18rem' }} className="book">
+      <Card.Body>
+        <Card.Title className="book-title">{bookname}</Card.Title>
+        <div className="book-details">
+          <div>Author: {author}</div>
+          <div>review: {review}</div>
+          <div>Date: {new Date(date).toDateString()}</div>
+        </div>
+        <Button variant="primary">Edit</Button>{' '}
+        <Button variant="danger" onClick={() => handleRemoveBook(id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Book;
